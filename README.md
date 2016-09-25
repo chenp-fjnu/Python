0f3d46eb11384936284eb645f2b4858f62937a9c
chenp-fjnu <chp0524@gmail.com>
2016-09-25 23:11:21 +0800

    working on http://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Python
 Working on Python
+http://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000/001431679203477b5b364aeba8c4e05a9bd4ec1b32911e2000
+
+
+# BuildIn functions
+https://docs.python.org/3/library/functions.html
 
 # Wiki
 https://docs.djangoproject.com/en/dev/
@@ -11,3 +16,24 @@ https://python-wordpress-xmlrpc.readthedocs.io/en/latest/index.html
 http://hyde.github.io/
 
 http://mezzanine.jupo.org/
+
+# Notes
+直接运行py文件
+
+有同学问，能不能像.exe文件那样直接运行.py文件呢？在Windows上是不行的，但是，在Mac和Linux上是可以的，方法是在.py文件的第一行加上一个特殊的注释：
+```python
+#!/usr/bin/env python3
+```
+print('hello, world')
+然后，通过命令给hello.py以执行权限：
+```unix
+$ chmod a+x hello.py
+```
+## Python代码运行助手
+```python
+python learning.py
+
+http://localhost:39093/
+```
+
+
---
+++ cookbook.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+print('100+200+300=',100+200+300)
+print('1024 * 768 =', 1024*768)
+print('''hello, world.
+I\'m "OK"''')
+
+def normalize(name):
+    return name.capitalize()
+L1 =['adam','LISA','braT']
+L2 = list(map(normalize, L1))
+print(L2)
+
+print('The quick brown fox', 'jumps over', 'the lazy dog')
+#name=input('please enter you name:')
+#print('Hello,', name)
+PI=3.1415926
+
+print(3>2 and not True)
+print(3>5 or True)
+print(10//3)
+print('Hello, %s, you have $%d' % ('Chet',1000))
+print('%2d-%03d' % (3,1.5))
+print('%.2f'% PI)
+s1 = 72
+s2 = 85
+r = (s2-s1)/s2 *100
+print('%.1f%%' % r)
+print('中文'.encode('utf_8'))
+print('中文'.encode('gb2312'))
+classmate=['Michael','Bob','Tracy']
+print(classmate)
+print(len(classmate))
+print(classmate[0])
+print(classmate[1])
+print(classmate[2])
+print(classmate[-1])
+classmate.append('Adam')
+classmate.insert(1,'Jack')
+print(classmate)
+print(classmate.pop())
+print(classmate)
+print(classmate.pop(1))
+print(classmate)
+s = ['python', 'java', ['asp', 'php'], 'scheme']
+print('%s. Length:%s' % (s,len(s)))
+classmate=('Michael','Bob','Tracy')
+print(classmate)
+
+for name in classmate:
+    print(name)
+t=(1)
+print(t)
+t=(1,)
+print(t)
+L = [
+    ['Apple', 'Google', 'Microsoft'],
+    ['Java', 'Python', 'Ruby', 'PHP'],
+    ['Adam', 'Bart', 'Lisa']
+]
+print(L[0][0])
+print(L[1][1])
+print(L[2][2])
+for item in L:
+    for subitem in item:
+        print(subitem)
+
+age=14 #int(input('birth:'))
+print('your age is %s' % age)
+if age >=18:  
+    print('adult')
+elif age>=6:
+    print('teenager')
+else:
+    print('kid')
+
+
+def calcBMI(_height, _weight):
+    bmi = _weight/(_height*_height)
+    print('your bmi is %s', bmi)
+    if bmi<=18.5:
+        return 'too light'
+    elif bmi<=25:
+        return 'normal'
+    elif bmi<=28:
+        return 'too heavy'
+    elif bmi<=32:
+        return 'fat'
+    else:
+        return 'too fat'
+height = 1.71
+weight = 72
+print(calcBMI(height,weight))
+
+sum=0
+for x in [1,2,3,4,5,6,7,8,9,10]:
+    sum=sum+x
+print(sum)
+print(list(range(101)))
+sum=0
+for x in range(101):
+    sum=sum+x
+print(sum)
+sum=0
+n=99
+while n>0:
+    sum=sum+n
+    n=n-2
+print(sum)
+L = ['Bart', 'Lisa', 'Adam']
+for name in L:
+    print(name)
+
+n=1
+while n<100:
+    if n>10:
+        break
+    print(n)
+    n=n+1
+print('END')
+n=0
+while n<10:
+    n=n+1
+    if n%2 ==0:
+        continue
+    print(n)
+d={'Michael':95, 'Bob':75, 'Tracy':85}
+print(d['Michael'])
+d['Jack'] =90
+print(d)
+if 'Thomas' in d:
+    print(True)
+else:
+    print(False)
+print(d.get('Tracy'))
+print(d.get('Non',-1))
+print(d.pop('Bob'))
+print(d)
+
+s=set([1,2,3])
+print(s)
+s=set([1,1,2,3,2,3])
+print(s)
+s.add(4)
+print(s)
+s.remove(3)
+print(s)
+s2=set([4,5,2])
+print(s & s2)
+print(s | s2)
+
+a =['c','b','a']
+print(a)
+a.sort()
+print(a)
+a = set((1,2,3))
+print(a)
+d= {'a':a}
+print(d)
+
+print(abs(-20))
+
+n1=255
+n2=1000
+print(hex(n1))
+print(hex(n2))
