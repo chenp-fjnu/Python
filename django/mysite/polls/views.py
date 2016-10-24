618035ca15135bd1aaf74c2b25322b1ec5c1bde6
chenp-fjnu <chp0524@gmail.com>
2016-10-24 21:23:50 +0800

    change to use generic view

--- django/mysite/polls/views.py
+++ django/mysite/polls/views.py
@@ -7,6 +7,7 @@ from .models import Question, Choice
 # from django.template import loader
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.views import generic
 
 def index(request):
     # return HttpResponse("Hello, world. You're at the polls index.")
@@ -41,3 +42,17 @@ def vote(request, question_id):
         selected_choice.votes+=1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+class IndexView(generic.ListView):
+    template_name='polls/index.html'
+    context_object_name = 'latest_question_list'
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name='polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model=Question
+    template_name='polls/results.html'
